ee4cc417c464502d643b987eee1e0b5c1cc59a29
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-06-22 14:31:00 +0000

    Update install_asdf_commands adding function named instalL_asdf_plugin_globally

--- bin/install_asdf_commands.sh
+++ bin/install_asdf_commands.sh
@@ -17,6 +17,19 @@ exist_asdf_plugin() {
   [[ "$(asdf plugin-list | grep "${plugin}" | wc -l)" -ne 0 ]]
 }
 
+install_asdf_plugin_globally() {
+  local plugin="$1"
+  local version="$2"
+
+  if ! exist_asdf_plugin "${plugin}"; then
+    asdf plugin-add "${plugin}"
+  fi
+  asdf install "${plugin}" "${version}"
+  asdf global "${plugin}" "${version}"
+}
+
+sudo apt update
+
 # asdf
 if [[ ! -d ~/.asdf ]]; then
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch "${ASDF_VERSION}"
@@ -32,36 +45,17 @@ asdf global nodejs "${NODE_VERSION}"
 npm install -g yarn
 
 # Python
-if ! exist_asdf_plugin python; then
-  asdf plugin-add python
-fi
-sudo apt update
 sudo apt install -y --no-install-recommends \
   make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev \
   wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-asdf install python "${PYTHON_VERSION}"
-asdf global python "${PYTHON_VERSION}"
+install_asdf_plugin_globally python "${PYTHON_VERSION}"
 
 # Ruby
-if ! exist_asdf_plugin ruby; then
-  asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-fi
 sudo apt install -y \
   autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
   libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
-asdf install ruby "${RUBY_VERSION}"
-asdf global ruby "${RUBY_VERSION}"
+install_asdf_plugin_globally ruby "${RUBY_VERSION}"
 
-# kubectl
-if ! exist_asdf_plugin kubectl; then
-  asdf plugin-add kubectl https://github.com/Banno/asdf-kubectl.git
-fi
-asdf install kubectl "${KUBECTL_VERSION}"
-asdf global kubectl "${KUBECTL_VERSION}"
-
-# kubectx
-if ! exist_asdf_plugin kubectx; then
-  asdf plugin-add kubectx
-fi
-asdf install kubectx "${KUBECTX_VERSION}"
-asdf global kubectx "${KUBECTX_VERSION}"
+# Kubernetes
+install_asdf_plugin_globally kubectl "${KUBECTL_VERSION}"
+install_asdf_plugin_globally kubectx "${KUBECTX_VERSION}"
