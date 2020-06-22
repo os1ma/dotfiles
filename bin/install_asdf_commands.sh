a98875dad0dd1cfd5298db394f435a497c0de4cf
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-06-22 14:02:21 +0000

    Update install_asdf_commands.sh to install Python and Ruby. Close #8

--- bin/install_asdf_commands.sh
+++ bin/install_asdf_commands.sh
@@ -7,6 +7,8 @@ set -o xtrace
 
 readonly ASDF_VERSION='v0.7.8'
 readonly NODE_VERSION='12.18.1'
+readonly PYTHON_VERSION='3.8.3'
+readonly RUBY_VERSION='2.7.1'
 
 exist_asdf_plugin() {
   local plugin="$1"
@@ -18,7 +20,7 @@ if [[ ! -d ~/.asdf ]]; then
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch "${ASDF_VERSION}"
 fi
 
-# node
+# Node.js
 if ! exist_asdf_plugin nodejs; then
   asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 fi
@@ -26,3 +28,24 @@ bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-ke
 asdf install nodejs "${NODE_VERSION}"
 asdf global nodejs "${NODE_VERSION}"
 npm install -g yarn
+
+# Python
+if ! exist_asdf_plugin python; then
+  asdf plugin-add python
+fi
+sudo apt update
+sudo apt install -y --no-install-recommends \
+  make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev \
+  wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+asdf install python "${PYTHON_VERSION}"
+asdf global python "${PYTHON_VERSION}"
+
+# Ruby
+if ! exist_asdf_plugin ruby; then
+  asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+fi
+sudo apt install -y \
+  autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
+  libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
+asdf install ruby "${RUBY_VERSION}"
+asdf global ruby "${RUBY_VERSION}"
