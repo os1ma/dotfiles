59c27c05e7156b597354fca87f0b5685a883d1f4
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-06-23 00:27:37 +0900

    Add gce_ubuntu/main.sh

---
+++ gce_ubuntu/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
+
+cd "${SCRIPT_DIR}"
+
+./install_asdf_commands.sh
+./install_tools.sh
+./setup_vnc.sh
