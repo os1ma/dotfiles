d80f42eb3977b060d08dd77f60f840d3f5f5f457
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-06-23 04:11:36 +0000

    Update main.sh to measure times. Close #58

--- gce_ubuntu/main.sh
+++ gce_ubuntu/main.sh
@@ -7,9 +7,11 @@ set -o xtrace
 
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
-cd "${SCRIPT_DIR}"
+main() {
+  time "${SCRIPT_DIR}"/install_asdf_commands.sh
+  time "${SCRIPT_DIR}"/install_cli_tools.sh
+  time "${SCRIPT_DIR}"/install_gui_tools.sh
+  time "${SCRIPT_DIR}"/setup_vnc.sh
+}
 
-./install_asdf_commands.sh
-./install_cli_tools.sh
-./install_gui_tools.sh
-./setup_vnc.sh
+time main "$@"
