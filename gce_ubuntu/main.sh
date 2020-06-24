8bb4a79b1a8f892f3a576c4f20522bb69342815d
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-06-24 20:05:21 +0900

    Update gce_ubuntu/main.sh not to install GUI tools and VNC

--- gce_ubuntu/main.sh
+++ gce_ubuntu/main.sh
@@ -10,8 +10,6 @@ readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 main() {
   time "${SCRIPT_DIR}"/install_asdf_commands.sh
   time "${SCRIPT_DIR}"/install_cli_tools.sh
-  time "${SCRIPT_DIR}"/install_gui_tools.sh
-  time "${SCRIPT_DIR}"/setup_vnc.sh
 }
 
 time main "$@"
