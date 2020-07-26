c31739c78374bc92a4b153588178f8cc79be42b1
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2020-07-26 11:54:30 +0000

    Add code-server settings.json

--- link.sh
+++ link.sh
@@ -5,6 +5,8 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+readonly CODE_SERVER_SETTING_DIR="${HOME}/.local/share/code-server/User/"
+
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
 ln -sf "${SCRIPT_DIR}"/.bashrc ~
@@ -13,3 +15,6 @@ ln -sf "${SCRIPT_DIR}"/.tmux.conf ~
 
 ln -sf "${SCRIPT_DIR}"/.tool-versions ~
 ln -sf "${SCRIPT_DIR}"/.asdfrc ~
+
+mkdir -p "${CODE_SERVER_SETTING_DIR}"
+ln -sf "${SCRIPT_DIR}"/settings.json "${CODE_SERVER_SETTING_DIR}"
