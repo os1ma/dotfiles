87b5752fe72eb0f3137213d0c8a5c3a354bc123f
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-05-11 14:51:36 +0900

    git のインストールを sudo で実行するよう変更

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-which git || (yum update -y && yum install -y git)
+which git || (sudo yum update -y && sudo yum install -y git)
 
 if [[ ! -d 'dotfiles' ]]; then
   git clone https://github.com/os1ma/dotfiles.git
