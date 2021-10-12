c4669c61ae97a12d595243e5e9f0b3a6f43dfe4c
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-10-12 10:15:42 +0900

    Add AWS default region setting

--- link.sh
+++ link.sh
@@ -16,5 +16,8 @@ ln -sf "${SCRIPT_DIR}"/.tmux.conf ~
 ln -sf "${SCRIPT_DIR}"/.tool-versions ~
 ln -sf "${SCRIPT_DIR}"/.asdfrc ~
 
+mkdir -p ~/.aws
+ln -sf "${SCRIPT_DIR}"/aws_config ~/.aws/config
+
 mkdir -p "${CODE_SERVER_SETTING_DIR}"
 ln -sf "${SCRIPT_DIR}"/settings.json "${CODE_SERVER_SETTING_DIR}"
