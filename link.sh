1e1cd2eabb348f651c297dfedf3ba9255f347176
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2025-02-08 09:39:35 +0900

    Add global gitignore setting

--- link.sh
+++ link.sh
@@ -5,10 +5,6 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-readonly CODE_SERVER_SETTING_DIR="${HOME}"/.local/share/code-server/User/
-readonly VSCODE_SETTING_DIR="${HOME}"/.config/Code/User/
-readonly MACOS_VSCODE_SETTING_DIR="${HOME}"/Library/Application\ Support/Code/User/
-
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
 ln -sf "${SCRIPT_DIR}"/.bash_profile ~
@@ -25,9 +21,18 @@ ln -sf "${SCRIPT_DIR}"/.asdfrc ~
 mkdir -p ~/.aws
 ln -sf "${SCRIPT_DIR}"/aws_config ~/.aws/config
 
+readonly CODE_SERVER_SETTING_DIR="${HOME}"/.local/share/code-server/User/
 mkdir -p "${CODE_SERVER_SETTING_DIR}"
 ln -sf "${SCRIPT_DIR}"/vscode/settings.json "${CODE_SERVER_SETTING_DIR}"
+
+readonly VSCODE_SETTING_DIR="${HOME}"/.config/Code/User/
 mkdir -p "${VSCODE_SETTING_DIR}"
 ln -sf "${SCRIPT_DIR}"/vscode/settings.json "${VSCODE_SETTING_DIR}"
+
+readonly MACOS_VSCODE_SETTING_DIR="${HOME}"/Library/Application\ Support/Code/User/
 mkdir -p "${MACOS_VSCODE_SETTING_DIR}"
 ln -sf "${SCRIPT_DIR}"/vscode/settings.json "${MACOS_VSCODE_SETTING_DIR}"
+
+readonly GITIGNORE_SETTING_DIR="${HOME}/.config/git/"
+mkdir -p "${GITIGNORE_SETTING_DIR}"
+ln -sf "${SCRIPT_DIR}/ignore" "${GITIGNORE_SETTING_DIR}"
